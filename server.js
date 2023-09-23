6a94f48b118b9ce3dd9eb93ab1a9bc74e5200a64
rahulgopireddy <rgopired@uncc.edu>
2023-09-22 23:31:10 -0400

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 const port = 3000;
 const chart_Data = require("./chartData.json");
 var pie_chartData = chart_Data;
